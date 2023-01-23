80e87ac70d94e35e924b411631b6a56666d864ce
Abdulkader-Safi <safi.abdulkader@gmail.com>
2023-01-23 17:56:57 +0200

    todo app with react Native 1

---
+++ todoList/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Tasks from "./components/Tasks";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          <Tasks text={"task 1"} />
+          <Tasks text={"task 2"} />
+          <Tasks text={"task 3"} />
+          <Tasks text={"task 4"} />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
+});

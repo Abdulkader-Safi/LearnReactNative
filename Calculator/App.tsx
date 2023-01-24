8ae3a7e58cb4b1c977a22b13997d508a49052e06
Abdulkader-Safi <safi.abdulkader@gmail.com>
2023-01-24 22:30:03 +0200

    Basic Calculater

---
+++ Calculator/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Text, View, Switch, SafeAreaView } from "react-native";
+import { ThemeContext } from "./src/context/ThemeContext";
+import { myColors } from "./src/styles/Colors";
+import MyKeyboard from "./src/components/MyKeyboard";
+import { Styles } from "./src/styles/GlobalStyles";
+
+export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView
+        style={
+          theme === "light" ? Styles.container : [Styles.container, { backgroundColor: "#000000" }]
+        }
+      >
+        <View style={Styles.setTheme}>
+          <Text style={theme === "light" ? { color: myColors.black } : { color: myColors.white }}>
+            Light Theme:{" "}
+          </Text>
+          <Switch
+            value={theme === "light"}
+            onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
+          />
+        </View>
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
+  );
+}
